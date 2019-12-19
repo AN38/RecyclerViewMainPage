ba549a97152438fd9a00ee17a74cef401d304533
AN38 <nikolskaya.a@chocolife.kz>
2019-12-19 20:35:09 +0600

    Finished

--- app/src/main/java/com/example/recyclerviewmainpage/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewmainpage/MainActivity.java
@@ -38,8 +38,8 @@ public class MainActivity extends AppCompatActivity {
         postModel.setCashbackAmount(7);
         postModel.setPeriod(10);
         postModel.setTransactionsAmount(800);
-        postModel.setImageUrl("http://www.spletnik.ru/img/__post/60/60cf0ac41d8c3404a26fb544a0887c1d_994.jpg");
-        postModel.setLogoUrl("https://pbs.twimg.com/profile_images/940390322198196224/mhjWgoNP_400x400.jpg");
+        postModel.setImageUrl("https://cdn.searchenginejournal.com/wp-content/uploads/2019/08/c573bf41-6a7c-4927-845c-4ca0260aad6b-760x400.jpeg");
+        postModel.setLogoUrl("https://www.corriereagrigentino.it/wp-content/uploads/2018/11/McDonalds.jpg");
         posts.add(postModel);
 
         PostModel postModel2 = new PostModel();
@@ -48,9 +48,29 @@ public class MainActivity extends AppCompatActivity {
         postModel2.setCashbackAmount(10);
         postModel2.setPeriod(10);
         postModel2.setTransactionsAmount(990);
-        postModel2.setImageUrl("http://www.sport-villa.kz/assets/images/slider/5_2.png");
-        postModel2.setLogoUrl("https://static.tildacdn.com/tild6263-3135-4634-b263-636235663636/Logo_URBO___2.png");
+        postModel2.setImageUrl("https://cdn.editorchoice.com/wp-content/uploads/2019/09/fast-food.jpg");
+        postModel2.setLogoUrl("https://icon2.cleanpng.com/20180202/pre/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083c57a5f5.317349121517619260359.jpg");
         posts.add(postModel2);
+
+        PostModel postModel3 = new PostModel();
+        postModel3.setFilialName("Chick inn");
+        postModel3.setCategoryName("Еда");
+        postModel3.setCashbackAmount(3);
+        postModel3.setPeriod(10);
+        postModel3.setTransactionsAmount(775);
+        postModel3.setImageUrl("https://f0.pngfuel.com/png/853/879/iced-coffee-latte-tea-cafe-cup-of-coffee-beans-black-ceramic-cup-filled-with-brown-coffee-png-clip-art-thumbnail.png");
+        postModel3.setLogoUrl("https://static.tildacdn.com/tild6263-3135-4634-b263-636235663636/Logo_URBO___2.png");
+        posts.add(postModel3);
+
+        PostModel postModel4 = new PostModel();
+        postModel4.setFilialName("Бабушкин буфет");
+        postModel4.setCategoryName("Еда");
+        postModel4.setCashbackAmount(8);
+        postModel4.setPeriod(10);
+        postModel4.setTransactionsAmount(325);
+        postModel4.setImageUrl("https://www.freepnglogos.com/uploads/food-png/download-food-png-file-png-image-pngimg-1.png");
+        postModel4.setLogoUrl("https://www.vippng.com/png/detail/26-261497_indian-cuisine-pakora-samosa-rajma-indian-food-cartoon.png");
+        posts.add(postModel4);
 
         return posts;
     }
